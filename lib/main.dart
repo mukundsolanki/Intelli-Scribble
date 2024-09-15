6ada1ce1a5f2f7faacea97c18f13e434972bcd28
Mukund Solanki <mukundsolanki665@gmail.com>
2024-09-15 12:13:15 +0530

    Different strokes and colors added

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,16 @@ class DrawingBoard extends StatefulWidget {
 }
 
 class _DrawingBoardState extends State<DrawingBoard> {
-  // List to store drawing points
-  List<Offset> points = [];
+  List<List<Offset>> strokes = [];
+  List<Color> strokeColors = [];
+  List<Offset>? currentStroke;
+  Color currentColor = Colors.black; // Default color is black
+
+  void _onColorSelected(Color color) {
+    setState(() {
+      currentColor = color;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,47 +35,117 @@ class _DrawingBoardState extends State<DrawingBoard> {
       body: GestureDetector(
         onPanUpdate: (details) {
           setState(() {
-            // Add the exact position of the user's touch or mouse movement to the points list
-            points.add(details.localPosition);
+            if (currentStroke == null) {
+              // Start a new stroke
+              currentStroke = [];
+              strokes.add(currentStroke!);
+              strokeColors.add(currentColor);
+            }
+            currentStroke!.add(details.localPosition);
           });
         },
         onPanEnd: (details) {
           setState(() {
-            points.add(Offset(-1, -1)); // Mark end of stroke with a sentinel value
+            if (currentStroke != null) {
+              currentStroke!.add(Offset(-1, -1)); // End of stroke
+              currentStroke = null; // Ready for new stroke
+            }
           });
         },
         child: CustomPaint(
-          painter: DrawingPainter(points),
-          size: Size.infinite, // The drawing area is as large as the screen
+          painter: DrawingPainter(strokes, strokeColors),
+          size: Size.infinite,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showMenu(
+            context: context,
+            position: RelativeRect.fromLTRB(
+              MediaQuery.of(context).size.width - 100,
+              MediaQuery.of(context).size.height - 100,
+              0,
+              0,
+            ),
+            items: [
+              PopupMenuItem<Color>(
+                value: Colors.red,
+                child: Container(
+                  width: 30,
+                  height: 30,
+                  color: Colors.red,
+                ),
+              ),
+              PopupMenuItem<Color>(
+                value: Colors.green,
+                child: Container(
+                  width: 30,
+                  height: 30,
+                  color: Colors.green,
+                ),
+              ),
+              PopupMenuItem<Color>(
+                value: Colors.blue,
+                child: Container(
+                  width: 30,
+                  height: 30,
+                  color: Colors.blue,
+                ),
+              ),
+              PopupMenuItem<Color>(
+                value: Colors.yellow,
+                child: Container(
+                  width: 30,
+                  height: 30,
+                  color: Colors.yellow,
+                ),
+              ),
+              PopupMenuItem<Color>(
+                value: Colors.purple,
+                child: Container(
+                  width: 30,
+                  height: 30,
+                  color: Colors.purple,
+                ),
+              ),
+            ],
+          ).then((selectedColor) {
+            if (selectedColor != null) {
+              _onColorSelected(selectedColor);
+            }
+          });
+        },
+        child: Icon(Icons.color_lens),
       ),
     );
   }
 }
 
 class DrawingPainter extends CustomPainter {
-  final List<Offset> points;
+  final List<List<Offset>> strokes;
+  final List<Color> strokeColors;
 
-  DrawingPainter(this.points);
+  DrawingPainter(this.strokes, this.strokeColors);
 
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 5.0
-      ..strokeCap = StrokeCap.round;
+    for (int i = 0; i < strokes.length; i++) {
+      Paint paint = Paint()
+        ..color = strokeColors[i]
+        ..strokeWidth = 5.0
+        ..strokeCap = StrokeCap.round;
 
-    // Loop through the points and draw
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != const Offset(-1, -1) && points[i + 1] != const Offset(-1, -1)) {
-        // Draw a line between consecutive points if they're not end-of-stroke markers
-        canvas.drawLine(points[i], points[i + 1], paint);
+      List<Offset> stroke = strokes[i];
+      for (int j = 0; j < stroke.length - 1; j++) {
+        if (stroke[j] != const Offset(-1, -1) && stroke[j + 1] != const Offset(-1, -1)) {
+          canvas.drawLine(stroke[j], stroke[j + 1], paint);
+        }
       }
     }
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true; // Redraw whenever new points are added
+    return true;
   }
 }
